172909a4242479a10ba7698143f0d9747cf2f8e2
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-26 11:49:55 -0300

    ex28.java

---
+++ ex28.java
@@ -0,0 +1,9 @@
+public class Ex28 {
+    public static void main(String[] args) {
+        System.out.println("Deque: permite inserção e remoção em ambas as extremidades (front e rear).");
+        System.out.println("Diferenças:");
+        System.out.println("- Pilha: apenas top (LIFO).");
+        System.out.println("- Fila: inserção no rear, remoção no front (FIFO).");
+        System.out.println("- Lista geral: permite operações em qualquer posição indexada; deque foca nas extremidades para eficiência.");
+    }
+}
