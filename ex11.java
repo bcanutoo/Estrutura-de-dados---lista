00e01147671673fde43eee47c5caf5ed5c54f9c2
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-26 11:26:56 -0300

    ex11.java

---
+++ ex11.java
@@ -0,0 +1,7 @@
+public class Ex11 {
+    public static void main(String[] args) {
+        System.out.println("Pilha: LIFO (inserção e remoção no topo).");
+        System.out.println("Fila: FIFO (inserção no fim, remoção no início).");
+        System.out.println("Deque: permite inserção e remoção em ambas as extremidades (front e rear).");
+    }
+}
