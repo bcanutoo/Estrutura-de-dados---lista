aa8748e748ffe94cbf64854b1d7fac366293050b
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-26 11:25:28 -0300

    ex10.java

---
+++ ex10.java
@@ -0,0 +1,6 @@
+public class Ex10 {
+    public static void main(String[] args) {
+        System.out.println("Listas seqüenciais gerais: armazenam elementos de forma sequencial e permitem inserção/remoção em qualquer posição (ex.: array/lista).");
+        System.out.println("Listas seqüenciais especiais: estruturas com restrições de acesso (ex.: pilha, fila, deque) onde posições de inserção/remoção são limitadas.");
+    }
+}
